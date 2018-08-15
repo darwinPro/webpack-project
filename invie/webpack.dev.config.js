1f751b3f84e6c329fe7a21067f3ad7cd1400dc43
darwin.da64 <darwin.DA64@gmail.com>
2018-08-15 13:51:24 -0500

    Finalizando el curso de webpack

---
+++ invie/webpack.dev.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        invie: path.resolve(__dirname, 'src/index.js')
+    },
+    output:{
+        path : path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'
+    },
+    devServer:{
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                //que tipo ed archivo quiero reconocer
+                test: /\.(js|jsx)$/,
+                //que loader se va a encargar del archivo
+                exclude: /(node_modules)/,
+                    
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react', 'stage-2']
+                    }
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1000000,
+                        fallback: 'file-loader',
+                        name: 'images/[name].[hash].[ext]'
+                    }
+                }
+            }
+        ]
+    }
+}
